5b150b15ae87bf082ec86c7181465bc177cb6c1e
Akhil Sanker <42499275+reekithak@users.noreply.github.com>
2020-10-02 05:15:17 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from flask import Flask
-from infer import * 
+#!/usr/bin/env python3
+from src import protein_prep, ligand_prep, perform_docking
+import os, tempfile
 
-app = Flask(__name__)
+home = os.path.expanduser("~")
 
-@app.route("/<cid>", methods=["GET"])
-def main(cid):
-    return predict(str(cid))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def execute(PDB_list: list, CID_list: list):
+    assert (os.path.isdir(
+        home + '/MGLTools-1.5.6')), "AutoDockTools not found! Script stopped"  # check if mgltools are present
+
+    for CID_entry in CID_list:
+        with tempfile.TemporaryDirectory(dir=os.getcwd()) as tmpdir:
+            ligand_prep.LigandPreparer(CID_entry, dir=tmpdir).prepare_ligand()
+            for PDB_entry in PDB_list:
+                protein_prep.ProteinPreparer(PDB_entry, dir=tmpdir).prepare_protein()
+                perform_docking.VinaDocker(protentry=PDB_entry,
+                                           ligentry=CID_entry,
+                                           protein_pdbqt=tmpdir + '/' + PDB_entry,
+                                           ligand_pdbqt=tmpdir + '/' + CID_entry,
+                                           dir=tmpdir
+                                           ).dock_merge_plip()
+
+
+if __name__ == '__main__':
+    ligand = ['11234', '666']
+    proteins = ['4pxz', '1llp', '1svk']
+    execute(proteins, ligand)
